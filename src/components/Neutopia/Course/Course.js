a95c51fafd0f252d35421968a69738ddde8d15fe
Vishnu V <vvishnu@hashinclude.io>
2023-12-28 18:14:00 +0530

    All Pushed

--- src/components/Neutopia/Course/Course.js
+++ src/components/Neutopia/Course/Course.js
@@ -8,6 +8,8 @@ function Course() {
     "https://firebasestorage.googleapis.com/v0/b/react-demo-788ae.appspot.com/o/Second-Set%2FplayConnect.jpeg?alt=media&token=55f70f0c-6bd7-4ec0-873e-e14dea2c7a7a",
     "https://firebasestorage.googleapis.com/v0/b/react-demo-788ae.appspot.com/o/Second-Set%2FhelpAlert.jpg?alt=media&token=3fade048-d0e8-4598-a8f3-db565f18de47",
     "https://firebasestorage.googleapis.com/v0/b/react-demo-788ae.appspot.com/o/Second-Set%2FsunniVoice.jpeg?alt=media&token=d337213b-d5cc-4d65-93d6-1ab52ad1b627",
+    "https://firebasestorage.googleapis.com/v0/b/react-demo-788ae.appspot.com/o/Second-Set%2Ftoppan.jpeg?alt=media&token=cf685428-c9bc-4639-9b4b-7e63133a6a28",
+    "https://firebasestorage.googleapis.com/v0/b/react-demo-788ae.appspot.com/o/Second-Set%2Fmaan.jpeg?alt=media&token=2f326c09-c613-4b31-8285-0cd0d78f1ec7"
   ];
   return (
     <div style={{paddingTop: '20px', paddingLeft:"20px",paddingRight:"20px"}}>
