bbff6e52d8c24d0148728eaae02779e52cdbcce3
Vishnu V <vvishnu@hashinclude.io>
2023-12-29 09:48:22 +0530

    All Pushed

--- src/components/Neutopia/Recently Viewed/RecentlyViewed.js
+++ src/components/Neutopia/Recently Viewed/RecentlyViewed.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { List, ListItem, Typography } from "@mui/material";
+import { List, ListItem, Typography, Stack } from "@mui/material";
 import RVCard from "../Recently Viewed Card/RVCard";
 
 function RecentlyViewed() {
@@ -14,26 +14,25 @@ function RecentlyViewed() {
     "https://firebasestorage.googleapis.com/v0/b/react-demo-788ae.appspot.com/o/Third-Set%2Fphoto6.jpeg?alt=media&token=b9470cd5-f436-4c65-b479-71fa71855f40",
     "https://firebasestorage.googleapis.com/v0/b/react-demo-788ae.appspot.com/o/Third-Set%2Fphoto7.jpeg?alt=media&token=a76c0829-38a1-4254-9e99-9a224ceda036",
     "https://firebasestorage.googleapis.com/v0/b/react-demo-788ae.appspot.com/o/Third-Set%2Fphoto8.jpeg?alt=media&token=86844abe-28d7-46ba-9351-8b8cfb1e05fb",
-    "https://firebasestorage.googleapis.com/v0/b/react-demo-788ae.appspot.com/o/Third-Set%2Fphoto8.jpeg?alt=media&token=f575978d-99be-4324-9993-cdd1ee7753a0"
+    "https://firebasestorage.googleapis.com/v0/b/react-demo-788ae.appspot.com/o/Third-Set%2Fphoto8.jpeg?alt=media&token=f575978d-99be-4324-9993-cdd1ee7753a0",
   ];
   return (
-    <div style={{ paddingTop: "20px" }}>
+    <div style={{ paddingTop: "20px",paddingLeft: "20px", paddingRight: "20px"  }}>
       <Typography
         style={{
           color: "#D5AF6C",
           fontSize: "24px",
           fontFamily: "ManropeMedium",
-          paddingLeft: "20px",
         }}
       >
         Random Clicks @ 2023
       </Typography>
       <List style={{ display: "flex", overflowX: "auto" }}>
-        {imageList.map((imgUrl, index) => (
-          <ListItem key={index}>
+        <Stack direction="row" spacing={3}>
+          {imageList.map((imgUrl) => (
             <RVCard imgUrl={imgUrl} />
-          </ListItem>
-        ))}
+          ))}
+        </Stack>
       </List>
     </div>
   );
